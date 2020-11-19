cbb4afca2b08ba3a84526408fbfc5c044a1768d9
Bloodsfire <voobsheeee@mail.ru>
2020-11-19 22:16:46 +0700

    Create Timer component

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import './Header.css';
+import { Timer } from "../timer/Timer";
 
 export function Header() {
 
     return (
         <div className="header">
-
+            <Timer />
         </div>
     );
 }
---
+++ src/features/timer/Timer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Timer.css';
+
+export function Timer() {
+
+    return (
+        <div className="timer">
+
+        </div>
+    );
+}
